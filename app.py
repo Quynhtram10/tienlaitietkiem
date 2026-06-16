4ea1bd6a902d94111fe035f5ffafebbeb222a239
Quynhtram10 <phanngocquynhtram0307@gmail.com>
2026-06-16 16:51:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.isst.image("IMG_4373.jpeg")
+st.ist.image("IMG_4373.jpeg")
 # Tiêu đề ứng dụng
 st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_Đề tài 5_ Phan Ngọc Quỳnh Trâm")
 
