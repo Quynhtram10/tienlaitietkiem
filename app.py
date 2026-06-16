2f68d8c675e2c9821d2580c1f6f010be7d0a694e
Quynhtram10 <phanngocquynhtram0307@gmail.com>
2026-06-16 16:48:39 +0700

    Update title and add image in Streamlit app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
+st.isst.image("IMG_4373.jpeg")
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_Đề tài 5_ Phan Ngọc Quỳnh Trâm")
 
 # Nhập dữ liệu
 C = st.number_input(
